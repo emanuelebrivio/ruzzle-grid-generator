1e262d133d375d9133617f6d691816ef5a3ecd60
Emanuele Brivio <emanuele@plasticpanda.com>
2015-07-20 12:54:33 +0200

    Working with multiple words

--- main.js
+++ main.js
@@ -1,92 +1,190 @@
 $(function () {
 
-  var words = ['CULTURE', 'MONOCHRONIC', 'POLICHRONIC', 'ARTEFACTS', 'VALUES', 'BEHAVIOURS', 'PROXEMICS', 'GLOBAL', 'RELATIONS', 'TASKS', 'ORIENTATION', 'TABOO', 'ADAPTATION', 'ATTITUDE', 'BELIEFS'];
+  var words = _.shuffle(['DIGITAL', 'COMMUNITY', 'ONLINE', 'COLLABORATION', 'TOOL', 'SOCIAL', 'NETWORK', 'UPDATE', 'TECHNOLOGY', 'CHAT', 'BLOG', 'FORUM', 'WIKI', 'AVATAR', 'SHARING', 'LIKE', 'EMAIL', 'MANAGER', 'MEDIA', 'STRATEGY', 'MODERATOR', 'AUTHOR', 'ADMIN', 'CULTURE', 'CODESIGN', 'CARE', 'ANALYTICS', 'LMS', 'GOVERNANCE', 'DATA', 'IOT']);
+
+  var retryCount = 50;
   var gridSize = 8;
-  
-  
-  
-  
-  var randohm = function (max) {
-    var newRandom = Math.floor((Math.random() * (max * 5)) % max);
-    console.log(newRandom);
-  }
-  
-  
-  
-  
   var grid = [];
+  var finalGrid = []
+  var inserted = [];
   
-  for (var i = 0; i < gridSize; i++) {
-    var row = [];
-    for (var j = 0; j < gridSize; j++) {
-      row.push('');
+  var printGrid = function () {
+    var gridsystem = '';
+    for (var i = 0; i < gridSize; i++) {
+      gridsystem += '<tr>';
+      for (var j = 0; j < gridSize; j++) {
+        gridsystem += '<td>' + ((finalGrid[i][j] == '' ? ' ' : finalGrid[i][j])) + '</td>';
+      }
+      gridsystem += '</tr>';
     }
-    grid.push(row);
+    document.getElementById('grid').innerHTML = gridsystem;
   }
   
-  console.log('Empty grid', grid);
+  var canGo = function (where, x, y, letter) {
+    if (where == 'right') {
+      y += 1;
+    }
+    if (where == 'left') {
+      y -= 1;
+    }
+    if (where == 'bottom') {
+      x += 1;
+    }
+    if (where == 'top') {
+      x -= 1;
+    }
+    
+    if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
+      if (grid[x][y] == '') {
+        return true;
+      }
+      if (letter && grid[x][y] == letter) {
+        return true;
+      }
+      return false;
+    } else {
+      return false;
+    }
+  }
   
-  //_.each(words, function (word) {
-    var word = 'CULTURE';
+  
+  var setupGrid = function () {
+    for (var i = 0; i < gridSize; i++) {
+      var row = [];
+      for (var j = 0; j < gridSize; j++) {
+        row.push('');
+      }
+      finalGrid.push(row);
+    }
+  };
+  
+  
+  
+  
+  setupGrid();
+  
+  _.each(words, function (word) {
+    
+    grid = _.clone(finalGrid, true);
+        
+    var flattenGrid = _.flatten(finalGrid);
+    var empty = [];    
+    _.map(flattenGrid, function (cell, id) { if (cell == '') { empty.push(id); }});
+
     console.log('Trying to insert Word', word);
     
-    function inject(word, index, directions, where, x, y) {
+    var count = 0;
+    
+    function inject(word, index, direction, x, y) {
+      
       if (index == word.length) {
+        
         // Word inserted
         console.log('Word inserted');
+        inserted.push(word);
+        finalGrid = _.clone(grid, true);
+        grid = [];
+        //printGrid();
+        
         return;
-      }
-      
-      if (where == directions.length) {
-      }
-      
-      if (grid[x][y] == '') {
-        grid[x][y] = word[index];
-        
-        if (directions[where] == 'right' && x != gridSize - 1 ||
-            directions[where] == 'left' && x != 0 ||
-            directions[where] == 'bottom' && y != gridSize - 1 ||
-            directions[where] == 'top' && y != 0) {
-          
-          // I can continue on this direction
-        
-        } else {
-          // Change direction
-          console.log('Change direction');
-          where += 1;
-        }
-          
-        if (directions[where] == 'right') {
-          inject(word, index + 1, directions, where, x + 1, y);
-        }
-        if (directions[where] == 'left') {
-          inject(word, index + 1, directions, where, x - 1, y);
-        }
-        if (directions[where] == 'bottom') {
-          inject(word, index + 1, directions, where, x, y + 1);
-        }
-        if (directions[where] == 'top') {
-          inject(word, index + 1, directions, where, x, y - 1);
-        }
         
       } else {
-        // Reset directions
-        console.log('Reset directions');
-        inject(word, index + 1, directions, 0, x, y);
+        
+        if ((x < 0 || x >= gridSize || y < 0 || y >= gridSize) || (grid[x][y] != '' && grid[x][y] != word[index])) {
+          
+          if (count <= retryCount) {
+            count ++;
+            console.log('Retry #' + count + ' of ' + retryCount, word);
+            grid = _.clone(finalGrid, true);
+            
+            var sample = _.sample(empty);
+            var sampleX = Math.floor(sample / gridSize);
+            var sampleY = sample % gridSize;
+
+            var direction = [];
+            if (canGo('top', sampleX, sampleY)) { direction.push('top'); }
+            if (canGo('left', sampleX, sampleY)) { direction.push('left'); }
+            if (canGo('right', sampleX, sampleY)) { direction.push('right'); }
+            if (canGo('bottom', sampleX, sampleY)) { direction.push('bottom'); }
+
+            console.log('I can go', direction, sampleX, sampleY);
+
+            inject(word, 0, _.sample(direction), sampleX, sampleY);
+            
+          } else {
+            console.log('Enough... change word');
+          }
+          return;
+        }
+        
+        grid[x][y] = word[index];
+
+        //console.log('Inserted', word[index]);
+
+
+        if (direction == 'right' && y != gridSize - 1 ||
+            direction == 'left' && y != 0 ||
+            direction == 'bottom' && x != gridSize - 1 ||
+            direction == 'top' && x != 0) {
+
+          // I can continue on this direction
+
+          if (direction == 'right') {
+            inject(word, index + 1, direction, x, y + 1);
+          }
+          if (direction == 'left') {
+            inject(word, index + 1, direction, x, y - 1);
+          }
+          if (direction == 'bottom') {
+            inject(word, index + 1, direction, x + 1, y);
+          }
+          if (direction == 'top') {
+            inject(word, index + 1, direction, x - 1, y);
+          }
+
+        } else {
+          var where;
+          if (direction == 'top' || direction == 'bottom') {
+            where = _.shuffle(['left', 'right']);
+          } else {
+            where = _.shuffle(['top', 'bottom']);
+          }
+          var newD = canGo(where[0], x, y, word[index + 1]) ? where[0] : where[1];
+
+          if (newD == 'right') {
+            inject(word, index + 1, newD, x, y + 1);
+          }
+          if (newD == 'left') {
+            inject(word, index + 1, newD, x, y - 1);
+          }
+          if (newD == 'bottom') {
+            inject(word, index + 1, newD, x + 1, y);
+          }
+          if (newD == 'top') {
+            inject(word, index + 1, newD, x - 1, y);
+          }
+        }
       }
     }
-    
-    inject(word, 0, ['top','left','right','bottom'], 0, 3, 4);
-    
-  //});
   
-  var gridsystem = '';
-  for (var i = 0; i < gridSize; i++) {
-    for (var j = 0; j < gridSize; j++) {
-      gridsystem += grid[i][j] + ',';
-    }
-    gridsystem += '\n';
-  }
-  console.log('gridsystem', gridsystem);
+    var sample = _.sample(empty);
+    var sampleX = Math.floor(sample / gridSize);
+    var sampleY = sample % gridSize;
+    
+    var direction = [];
+    if (canGo('top', sampleX, sampleY)) { direction.push('top'); }
+    if (canGo('left', sampleX, sampleY)) { direction.push('left'); }
+    if (canGo('right', sampleX, sampleY)) { direction.push('right'); }
+    if (canGo('bottom', sampleX, sampleY)) { direction.push('bottom'); }
+    
+    console.log('I can go', direction, sampleX, sampleY);
+    
+    inject(word, 0, _.sample(direction), sampleX, sampleY);
+    
+  });
+  
+  
+  console.log('Inserted words', inserted.length, inserted);
+  printGrid();
   
 });
